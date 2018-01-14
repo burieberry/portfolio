3a527cf7a2a4cf0732353c015fbd667b4067f601
Burcu Noyan <burcunoyan@gmail.com>
2018-01-14 18:08:17 -0500

    add css grid

---
+++ src/Grid.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './grid.css';
+
+const Grid = () => {
+
+  return (
+    <div className="container">
+      <div className="one">One</div>
+      <div className="two">Two</div>
+      <div className="three">Three</div>
+      <div className="four">Four</div>
+      <div className="five">Five</div>
+      <div className="six">Six</div>
+    </div>
+  );
+}
+
+export default Grid;
