9aadb036a22b41f6b90b7f58c874c5280c05799a
Burcu Noyan <burcunoyan@gmail.com>
2018-01-27 22:29:54 -0500

    add Intro with h1 and title

---
+++ src/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Burcu Noyan</h1>
+      <h3>Full Stack Software Engineer</h3>
+    </div>
+  )
+}
+
+export default Intro;
