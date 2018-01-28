b4efeca93b51f81bd3d79a98678a47aa1fe052ab
Burcu Noyan <burcunoyan@gmail.com>
2018-01-28 01:36:33 -0500

    create Nav component with initial nav items

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './nav.css';
+
+const Nav = () => {
+  return (
+    <div className='navbar'>
+      <span className='nav-item'>Home</span>
+      <span className='nav-item'>Portfolio</span>
+      <span className='nav-item'>Contact</span>
+    </div>
+  );
+}
+
+export default Nav;
