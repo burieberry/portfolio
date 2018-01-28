c6f0f13d0b4fe5d24020c8b677b2c4473b005b47
Burcu Noyan <burcunoyan@gmail.com>
2018-01-28 01:38:09 -0500

    create Projects component

---
+++ src/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './projects.css';
+
+const Projects = ({ title }) => {
+  return (
+    <div>
+      <h2 className='subhead'>{ title }</h2>
+      <ul>
+        {/* projects TK */}
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
