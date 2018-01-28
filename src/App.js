67cbb1b5f495743f02b0dfbfa5f88b0d432508f5
Burcu Noyan <burcunoyan@gmail.com>
2018-01-28 00:08:06 -0500

    remove logo, reduce header height

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Grid from './Grid';
@@ -8,8 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <header className="App-header"></header>
         <Grid />
       </div>
     );
