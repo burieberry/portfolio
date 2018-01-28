c739f4a519ca934adc5b6bc762d4d8c35369088f
Burcu Noyan <burcunoyan@gmail.com>
2018-01-27 22:38:49 -0500

    add intro description

--- src/Grid.js
+++ src/Grid.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Intro from './Intro';
+import IntroBlurb from './IntroBlurb';
 
 import './grid.css';
 
@@ -11,7 +12,9 @@ const Grid = () => {
       <div className="one">
         <Intro />
       </div>
-      <div className="two">Two</div>
+      <div className="two">
+        <IntroBlurb />
+      </div>
       <div className="three">Three</div>
       <div className="four">Four</div>
       <div className="five">Five</div>
---
+++ src/IntroBlurb.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IntroBlurb = () => {
+  return (
+    <div>
+      <h2>Hi, my name is Burcu Noyan. I am a full stack software engineer with a passion for front-end development and design.</h2>
+      <h2>pronounced: 'bu or ju'</h2>
+    </div>
+  )
+}
+
+export default IntroBlurb;
