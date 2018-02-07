49ac18274fe18d8ad3753e16cb3794ea37f6d5e1
Burcu Noyan <burcunoyan@gmail.com>
2018-02-07 02:38:07 -0500

    put projects in allProjects

--- src/Projects.js
+++ src/Projects.js
@@ -3,19 +3,26 @@ import React from 'react';
 import './projects.css';
 
 import Project from './Project';
-import projectImg from './images/ratingfinder.png';
+import allprojects from './allProjects';
+// import projectImg from './images/ratingfinder.png';
 
 const Projects = ({ title }) => {
-  // TODO: import these from JSON obj, incl the image
-  const projectTitle = 'RatingFinder';
-  const projectDesc = 'Find and compare ratings and reviews from Yelp, Google Maps, Foursquare — all in one place.';
-
   return (
     <div>
       <h2 className='subhead'>{ title }</h2>
       <ul className='project_container'>
-        <li className='project'><Project projectTitle={ projectTitle } projectDesc={ projectDesc } projectImg={ projectImg } /></li>
-        <li className='project'><Project /></li>
+        {
+          allprojects.map(proj => {
+            return (
+              <li className='project'>
+                <Project
+                  projectTitle={ proj.projectTitle }
+                  projectDesc={ proj.projectDesc }
+                  projectImg={ proj.projectImg } />
+              </li>
+            );
+          })
+        }
       </ul>
     </div>
   );
---
+++ src/allProjects.js
@@ -0,0 +1,26 @@
+import ratingfinderImg from './images/ratingfinder.png';
+
+const AllProjects = [
+  {
+    projectTitle: 'RatingFinder',
+    projectDesc: 'Find and compare ratings and reviews from Yelp, Google Maps, Foursquare — all in one place.',
+    projectImg: ratingfinderImg
+  },
+  {
+    projectTitle: 'RatingFinder',
+    projectDesc: 'Find and compare ratings and reviews from Yelp, Google Maps, Foursquare — all in one place.',
+    projectImg: ratingfinderImg
+  },
+  {
+    projectTitle: 'RatingFinder',
+    projectDesc: 'Find and compare ratings and reviews from Yelp, Google Maps, Foursquare — all in one place.',
+    projectImg: ratingfinderImg
+  },
+  {
+    projectTitle: 'RatingFinder',
+    projectDesc: 'Find and compare ratings and reviews from Yelp, Google Maps, Foursquare — all in one place.',
+    projectImg: ratingfinderImg
+  }
+];
+
+export default AllProjects;
