bf475c81c166aba0c72eda6cd0206a6ac88916ef
Burcu Noyan <burcunoyan@gmail.com>
2018-02-07 01:53:48 -0500

    install fa icons; add to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter, faLinkedinIn, faMediumM, faStackOverflow } from '@fortawesome/fontawesome-free-brands';
+import { faCopyright, faEnvelope } from '@fortawesome/fontawesome-free-solid';
 
 import './footer.css';
 
 const Footer = ({ title }) => {
+  const icons = [
+    faLinkedinIn,
+    faGithub,
+    faTwitter,
+    faStackOverflow,
+    faMediumM,
+    faEnvelope
+  ]
+
   return (
     <div className='footer'>
-      <h2 className='subhead'>{ title }</h2>
-      <p className='copyright'><small>©</small> Site built and designed by Burcu Noyan, 2018.</p>
+      {/*<h2 className='subhead'>{ title }</h2>*/}
+      <div className='copyright'>
+        <p><small>©</small> Site built by Burcu Noyan, 2018.</p>
+      </div>
+      <div className='social-buttons'>
+        {
+          icons.map(icon => {
+            return (
+              <FontAwesomeIcon className='icon' icon={ icon } />
+            );
+          })
+        }
+      </div>
     </div>
   );
 }
