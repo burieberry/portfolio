bdc51aa3f409a58d712fbcd9562393f60a378906
Burcu Noyan <burcunoyan@gmail.com>
2018-02-06 23:42:40 -0500

    add Material design button and style

--- src/Project.js
+++ src/Project.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Button } from 'rmwc/Button';
 
+import '@material/button/dist/mdc.button.min.css';
 import './project.css';
 
+
 const Project = ({ projectTitle, projectDesc, projectImg }) => {
+  const backgroundColor = '#674497';
+
   return (
     <div className='project-box'>
       <img className='project-img' src={ projectImg } alt='' />
       <h3 className='subhead project-title'>{ projectTitle }</h3>
       <p className='project-desc'>{ projectDesc }</p>
       <div className='project-buttons'>
-        <button>Demo</button>
-        <button>Code</button>
+        <Button raised dense style={{ backgroundColor }}>
+          <span className="project-btn">Demo</span>
+        </Button>
+        <Button raised dense style={{ backgroundColor }}>
+          <span className="project-btn">Code</span>
+        </Button>
       </div>
     </div>
   );
