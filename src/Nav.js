6e343e2f7c1c87129ab57b4d0b84aef2fe13d6af
Burcu Noyan <burcunoyan@gmail.com>
2018-02-07 20:14:34 -0500

    add NavLinks and style

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './nav.css';
 
 const Nav = () => {
   return (
     <div className='navbar'>
-      <span className='nav-item'>Home</span>
-      <span className='nav-item'>Portfolio</span>
-      <span className='nav-item'>Contact</span>
+      <NavLink to='/' className='nav-item'>Home</NavLink>
+      <NavLink to='/projects' activeClassName='selected' className='nav-item'>Portfolio</NavLink>
+      <NavLink to='/contact' activeClassName='selected' className='nav-item'>Contact</NavLink>
     </div>
   );
 }
