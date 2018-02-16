44dab035c306d27c67c103d1c4caa8bbb6f4950d
Burcu Noyan <burcunoyan@gmail.com>
2018-02-16 00:58:02 -0500

    update Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { faGithub, faTwitter, faLinkedinIn, faMediumM, faStackOverflow } from '@fortawesome/fontawesome-free-brands'
-import { faEnvelope } from '@fortawesome/fontawesome-free-solid';
 import { Link } from 'react-router-dom';
+
+import { ContactLinks } from './socialLinks';
 
 import '@material/button/dist/mdc.button.min.css';
 import './footer.css';
 
 const Footer = ({ title }) => {
-  const icons = [
-    { icon: faLinkedinIn,
-      link: 'https://www.linkedin.com/in/burcunoyan/'
-    },
-    { icon: faGithub,
-      link: 'https://github.com/burieberry'
-    },
-    { icon: faTwitter,
-      link: 'https://twitter.com/burieberry'
-    },
-    { icon: faStackOverflow,
-      link: 'https://stackoverflow.com/users/8845312/burieberry'
-    },
-    { icon: faMediumM,
-      link: 'https://medium.com/@Burieberry'
-    },
-    { icon: faEnvelope,
-      link: 'mailto:burcunoyan@gmail.com'
-    }
-  ];
-
   const copyrightTxt = 'Site built by Burcu Noyan, 2018.';
 
   return (
@@ -39,7 +18,7 @@ const Footer = ({ title }) => {
       </div>
       <div className='social-buttons'>
         {
-          icons.map(btn => {
+          ContactLinks.map(btn => {
             return (
               <Link to={ btn.link } target='_blank'>
                 <FontAwesomeIcon key={ btn.iconName } className='footer-icon' icon={ btn.icon } />
